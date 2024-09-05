eed5889fd47bc0db52d6230d70b4ed6039cc12d3
Zac-Milioli <zacmilioli@gmail.com>
2024-09-05 16:11:23 -0300

    página oh yeah

---
+++ passpass-landpage/src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Zac</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
